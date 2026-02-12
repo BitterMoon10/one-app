edb4155a32d8333164ef89a74c0a3f572db434a4
zhangqinglei <zhangqinglei@msh.team>
2026-02-12 21:00:03 +0800

    Use HashRouter for GitHub Pages compatibility

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import Navigation from './components/Navigation';
 import Home from './pages/Home';
 import Photography from './pages/Photography';
