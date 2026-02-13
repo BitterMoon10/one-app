9895d65d0b00d1fd22c4c8cfdb094928a091bb7c
zhangqinglei <zhangqinglei@msh.team>
2026-02-13 16:47:14 +0800

    Make navigation background fully transparent

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -33,7 +33,7 @@ const Navigation = () => {
         className={`fixed top-0 left-0 right-0 z-50 transition-all duration-500 ${
           isHomePage && !scrolled 
             ? 'bg-transparent' 
-            : 'bg-white/90 shadow-sm'
+            : 'bg-transparent'
         }`}
       >
         <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
